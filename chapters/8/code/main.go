4ad88c7518cea8affa4ce4e48d7a406039ff3428
a1ex-var1amov <varlamov.it@gmail.com>
2024-12-15 14:40:31 -0500

    chapter 4

---
+++ chapters/8/code/main.go
@@ -0,0 +1,20 @@
+package main 
+ 
+import ( 
+  "fmt" 
+  "net/http" 
+  "time" 
+) 
+
+ 
+
+func main() { 
+  http.HandleFunc("/", HelloServer) 
+  http.ListenAndServe("0.0.0.0:8080", nil) 
+} 
+
+
+func HelloServer(w http.ResponseWriter, r *http.Request) { 
+  currentTime := time.Now().Format("2006-01-02 15:04:05") 
+  fmt.Fprintf(w, "Hello Student! The request time is %s.", currentTime) 
+} 
