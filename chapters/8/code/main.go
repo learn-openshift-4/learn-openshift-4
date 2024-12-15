028cde5c8f712b7b4b5125c191db9bc3f34b8adc
a1ex-var1amov <varlamov.it@gmail.com>
2024-12-15 14:43:35 -0500

    chapter 8

--- chapters/8/code/main.go
+++ chapters/8/code/main.go
@@ -5,14 +5,11 @@ import (
   "net/http" 
   "time" 
 ) 
-
  
-
 func main() { 
   http.HandleFunc("/", HelloServer) 
   http.ListenAndServe("0.0.0.0:8080", nil) 
 } 
-
 
 func HelloServer(w http.ResponseWriter, r *http.Request) { 
   currentTime := time.Now().Format("2006-01-02 15:04:05") 
